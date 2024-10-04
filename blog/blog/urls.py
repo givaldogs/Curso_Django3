cf72f9780c9ec9bfaa0c7e9dddf16bf524557262
givaldogs <givaldowanda@gmail.com>
2024-10-04 12:01:54 -0300

    Aula 14- Personalizando o Django admin e utilizando novo recurso da versao 3

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import hello_world, current_datetime
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
     path('dataHora/', current_datetime),
+    path('blog/', include('website.urls')),
 ]
 
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def hello_blog(request):
+      list = [
+            'Django', 'Python', 'Git', 'Html', 
+            'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi',
+            'Systemclt'
+      ]
+      list_posts = Post.objects.all()
+      data ={'name': 'Curso de Django 3', 'lista_tecnologias':list,
+             'posts': list_posts
+             }
+      return render(request, 'index.html', data)
+
+    # return render(request, 'indexgeral.html')
+    #return HttpResponse('Blog')
