ca22d5c9e8fb869c8526cbd1a9d0aa9fd5ecaa40
givaldogs <givaldowanda@gmail.com>
2024-10-05 19:56:07 -0300

    Utilizando um novo TextChoices da versao 3

--- blog/website/models.py
+++ blog/website/models.py
@@ -2,10 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Categories(models.TextChoices):
+    TECH = "TC", 'Tecnologia'
+    SALAOBLZ  = 'BE', 'Salao Beleza'
+    CR = 'CR', 'Curiosidade'
+    GR = 'GR', 'Geral'
+    
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title= models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categories.choices,
+        default=Categories.GR,
+    )
 
     def __str__(self):
         return self.title
