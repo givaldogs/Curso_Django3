51eb10de1ee84a6cc958f8c3e976f8704697d9e7
givaldogs <givaldowanda@gmail.com>
2024-10-31 15:16:51 -0300

    arquivos de imagem

--- blog/urls.py
+++ blog/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import hello_world, current_datetime
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
     path('dataHora/', current_datetime),
     path('blog/', include('website.urls')),
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Contact
 
 # Create your views here.
 
@@ -9,7 +9,7 @@ def hello_blog(request):
             'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi',
             'Systemclt'
       ]
-      list_posts = Post.objects.all()
+      list_posts = Post.objects.filter(deleted=False)
       data ={'name': 'Curso de Django 3', 'lista_tecnologias':list,
              'posts': list_posts
              }
@@ -17,3 +17,20 @@ def hello_blog(request):
 
     # return render(request, 'indexgeral.html')
     #return HttpResponse('Blog')
+
+def post_detail(request, id):
+      post = Post.objects.get(id=id)
+      return render(request, 'post_detail.html',{'post': post})
+
+def save_form(request):
+      name=request.POST['name']
+      Contact.objects.create(
+            name=name,
+            email=request.POST['email'],
+            message=request.POST['message']
+      )
+      return render(request, 'contact_success.html', {'name_contact': name})
+
+def contato(request):
+      return render(request, 'contato.html')
+
